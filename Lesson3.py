355d1b50ba3c9a45cc5ee33d2ca853aee0202a2b
jgray <hang10jeff@gmail.com>
2017-08-24 11:09:55 -0600

    first try

---
+++ Lesson3.py
@@ -0,0 +1,14 @@
+wind = eval(input("Enter wind speed: "))
+
+if wind >= 74 and wind <= 95:
+    print ("Cat1")
+elif wind >= 96 and wind <= 110
+    print ("Cat2")
+elif wind >= 111 and wind <= 130
+    print ("Cat3")
+elif wind >= 131 and wind <= 155
+    print ("Cat4")
+elif wind >= 156
+    print ("Cat5")
+else
+    print ("N/A")
